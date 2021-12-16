d98beb118dfb96626cff597ba4621c648264c4f3
Sam Smith <samsmithnz@gmail.com>
2021-12-16 07:44:59 -0500

    Added secret in appsettings file

--- src/DependabotSecretsPOC.Tests/SecretsTests.cs
+++ src/DependabotSecretsPOC.Tests/SecretsTests.cs
@@ -1,4 +1,6 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace DependabotSecretsPOC.Tests;
 
@@ -6,12 +8,25 @@ namespace DependabotSecretsPOC.Tests;
 public class SecretsTests
 {
     [TestMethod]
-    public void SecretTest()
+    public void StringTest()
     {
         //arrange
         string secret = "abc123";
 
         //assert
         Assert.AreEqual("abc123", secret);
+    }
+
+    [TestMethod]
+    public void SecretTest()
+    {
+        //arrange
+        IConfigurationBuilder config = new ConfigurationBuilder()
+           .SetBasePath(AppContext.BaseDirectory)
+           .AddJsonFile("appsettings.json");
+        IConfigurationRoot Configuration = config.Build();
+
+        //assert
+        Assert.AreEqual("abc123",Configuration["AppSettings:MySecret"]);
     }
 }
