49e7f075780ff9b2e0652f0f7e5e183d7f1d0252
Sam Smith <samsmithnz@gmail.com>
2021-12-15 22:55:26 -0500

    Added new project

---
+++ src/DependabotSecretsPOC.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DependabotSecretsPOC.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
